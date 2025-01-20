cc73c99d16b0d535e68b9ae19a08aa49bb6d8c76
ankit <ankitray0308@gmail.com>
2025-01-20 15:03:00 +0530

    Changes in generateToken

--- api/utils/generateToken.js
+++ api/utils/generateToken.js
@@ -2,7 +2,10 @@ const jwt = require("jsonwebtoken");
 
 // genrate JWT token
 function generateToken(id) {
-  return jwt.sign({ id }, process.env.JWT_SECRET, { expiresIn: "30d" });
+  return jwt.sign({ id }, process.env.JWT_SECRET, {
+    expiresIn: "30d",
+    secure: true,
+  });
 }
 
 module.exports = generateToken;
